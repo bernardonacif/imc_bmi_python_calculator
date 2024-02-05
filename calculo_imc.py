0d0777f0b924c694f9c45e268b07ab204284d28a
Bernardo Nacif <bernardonacif@MacBook-Pro-de-Bernardo.local>
2024-02-05 19:55:33 -0300

    feat: add SQLite settings

--- calculo_imc.py
+++ calculo_imc.py
@@ -3,11 +3,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from prettytable import PrettyTable
+import yaml
+import sqlite3
+
+# Carregar dados YAML do arquivo
+with open('./cfg/appsettings.yml', 'r') as arquivo:
+    cfg_data = yaml.load(arquivo, Loader=yaml.FullLoader)
+
+
+unit_type = cfg_data['user_cfg']['unit']
 
 altura = 1.68
 peso_usuario = 60
 imc_usuario = peso_usuario / (altura * altura)
 usuario = 'usuário_teste'
+user_email = 'user4.test@tesmail.com'
+
+
+if cfg_data['sql_cfg']['use_sql'] == 1:
+    try:
+        conn = sqlite3.connect('./SQLite/imc_bmi.db')
+        cursor = conn.cursor()
+        #verificar se o usr existe
+        cursor.execute('''select email, id from user where email = ?''',(user_email,))
+        existing_user = cursor.fetchone()
+        if existing_user is None:
+            try:
+                # Se o usuário não existir, inserir um novo usuário
+                cursor.execute('''INSERT INTO "USER" ("name", "email", "create")
+                                  VALUES (?, ?, CURRENT_TIMESTAMP)''', (usuario, user_email))
+                conn.commit()
+                cursor.execute('''SELECT last_insert_rowid()''')
+                user_id = cursor.fetchone()[0]
+            except sqlite3.Error as e:
+                print(f'Erro: SQLite insert error. {e}')          
+        else:
+            email, user_id = existing_user 
+    except sqlite3.Error as e:
+       print(f'Error to connect database. {e}')
+    finally:
+       conn.close()
+# else:
+#    print('sql config OFF')
+    
 
 top_range_weight = 121
 if peso_usuario >= 120:
@@ -17,6 +55,12 @@ if peso_usuario >= 120:
 # Lista para armazenar os dados
 dados = []
 ideal_weight = []
+
+# Underweight = <18.5
+# Normal weight = 18.5–24.9
+# Overweight = 25–29.9
+# Obesity = BMI of 30 or greater
+
 
 for i in range(0, top_range_weight):
     imc = i / (altura * altura)
@@ -47,12 +91,31 @@ df = pd.DataFrame(dados)
 df_ideal_weight = pd.DataFrame(ideal_weight)
 # print(df)
 
+# Gravar dados do usuario na tbl SQLite
+
+categoria_usuario = df.loc[df['IMC'] <= imc_usuario, 'Categoria'].iloc[-1]
+
+if cfg_data['sql_cfg']['use_sql'] == 1:
+    try:
+        conn = sqlite3.connect('./SQLite/imc_bmi.db')
+        cursor = conn.cursor()
+        try:
+            # report_id|user_id|weight|height|imc_bmi|category|measures_type|date|
+            cursor.execute('''INSERT INTO "user_reports" ("user_id", "weight", "height", "imc_bmi", "category", "measures_type", "date")
+          VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)''', (user_id, peso_usuario, altura, imc_usuario, categoria_usuario, unit_type))
+            conn.commit()
+        except sqlite3.Error as e:
+                   print(f'Erro: SQLite insert error. {e}')     
+    except sqlite3.Error as e:
+       print(f'Error to connect database. {e}')
+    finally:
+       conn.close()
+
 # Modelando Tabela 2
 round_peso_usuario = round(peso_usuario, 0)
 peso_base = round_peso_usuario - 10
 peso_top = round_peso_usuario + 10
 
-categoria_usuario = df.loc[df['IMC'] <= imc_usuario, 'Categoria'].iloc[-1]
 
 if categoria_usuario != 'intervalo normal':
   # descobrindo peso ideal
@@ -82,20 +145,20 @@ print('Tabela 1: Pesos ideais.')
 
 tabela_pretty = PrettyTable(df_ideal_weight.columns.tolist())
 tabela_pretty.add_rows(df_ideal_weight.values)
-print(tabela_pretty)
+# print(tabela_pretty)
 
 print('Tabela 2: Comparativo.')
 
 tabela_pretty = PrettyTable(df_filter.columns.tolist())
 tabela_pretty.add_rows(df_filter.values)
-print(tabela_pretty)
+# print(tabela_pretty)
 
 print('Tabela 3: Dados usuário.')
 
 x = PrettyTable()
 x.field_names = ["Nome", "Peso", "IMC", "Categoria"]
 x.add_row([usuario, peso_usuario, round(imc_usuario, 2), categoria_usuario])
-print(x)
+# print(x)
 
 
 # Ajuste df para indices do grafico
@@ -130,4 +193,4 @@ ax.legend()
 ax.set_xticks(range(0, top_range_weight, 10))
 
 # Exibir o gráfico
-plt.show()
+# plt.show()
